5dd3913023a50ef9e6b48e38b0956fdd0c881a97
Apisathan Sivananthanathan <apisathan2000@gmail.com>
2025-08-01 10:08:49 +0530

    rate limiter added

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 import app from "./app.js";
 import "dotenv/config";
 import redisClient from "./config/redisConfig.js";
-
+import notFound from "./middleware/notFound.js";
 
 import weatherRouter from "./routes/weatherRoutes.js";
+
+import rateLimit from "express-rate-limit";
+
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: { error: "Too many requests, please try again later." }
+});
+
+app.use("/api/", limiter);
 
 app.use(`/api/v1`, weatherRouter);
 
@@ -11,17 +24,16 @@ app.get("/api/v1/test", (req, res) => {
   return res.status(200).json({ msg: `Weather API !` });
 });
 
+app.use(`/`, notFound);
+
 const start = async () => {
   const port = process.env.PORT || 3000;
 
   try {
+    await redisClient.connect();
     app.listen(port, () => {
       console.log(`App is listening on port ${port}`);
     });
-
-    await redisClient.connect();
-    
-
   } catch (error) {
     console.log(error);
   }
